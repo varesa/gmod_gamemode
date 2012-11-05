924c92fa0e5b140727d2a3c39fdb51877e259673
Esa Varemo <esa@kuivanto.fi>
2012-11-05 22:58:03 +0200

    Handle not-yet-started timers correctly

--- gamemode/cl_hud.lua
+++ gamemode/cl_hud.lua
@@ -8,10 +8,13 @@ surface.CreateFont ("testFont", {
 
 function PaintHUD()
 	ply = LocalPlayer()
-	if ply:GetNWInt( "StopwatchEnd", 0 ) > 0  then
+	if ply:GetNWInt( "StopwatchEnd", -1 ) > 0  then
 		time = ply:GetNWInt( "StopwatchEnd", 0 ) - ply:GetNWInt( "StopwatchBegin", 0 )
-	else
+	elseif ply:GetNWInt( "StopwatchBegin", -1 ) > 0 then
 		time = RealTime() - LocalPlayer():GetNWInt("StopwatchBegin", 0)
+	else
+		draw.DrawText("Timer not started", "testFont", ScrW() / 2 - 70, ScrH() - 63, Color(255, 255, 255, 255),TEXT_ALIGN_CENTER)
+		return
 	end
 	
 	mins = math.floor(time / 60)
